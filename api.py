528e8efe53c39719b06045f2880eb01efc8c03bb
Chodoro <teodorejames@gmail.com>
2024-05-24 15:30:54 +0800

    fixed create, read, update, and delete functions

--- api.py
+++ api.py
@@ -3,13 +3,11 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# Required
+# Database configuration
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "root"
 app.config["MYSQL_DB"] = "company"
-
-# Extra configs, optional:
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
@@ -21,10 +19,18 @@ def fetch_data(query, args=()):
     cur.close()
     return data
 
+def execute_query(query, args=()):
+    cur = mysql.connection.cursor()
+    cur.execute(query, args)
+    mysql.connection.commit()
+    affected_rows = cur.rowcount
+    cur.close()
+    return affected_rows
+
 @app.route("/")
 def welcome():
     return """<h1>Employee Database</h1>
-    <p>Click <a href="http://127.0.0.1:5000/employees">here</a> to view all employees</p>"""
+    <p>Click <a href="/employees">here</a> to view all employees</p>"""
 
 @app.route("/employees", methods=["GET"])
 def get_employees():
@@ -38,31 +44,25 @@ def get_employees():
 def get_employee_by_ssn(ssn):
     try:
         data = fetch_data("SELECT * FROM employee WHERE ssn = %s", (ssn,))
-        return make_response(jsonify(data), 200)
+        if data:
+            return make_response(jsonify(data), 200)
+        else:
+            return make_response(jsonify({"Error": "Employee not found"}), 404)
     except Exception as e:
         return make_response(jsonify({"Error": str(e)}), 500)
 
 @app.route("/employees", methods=["POST"])
 def add_employee():
     try:
-        cur = mysql.connection.cursor()
         info = request.get_json()
-        fname = info["Fname"]
-        minit = info["Minit"]
-        lname = info["Lname"]
-        bdate = info["Bdate"]
-        address = info["Address"]
-        sex = info["Sex"]
-        salary = info["Salary"]
-        super_ssn = info["Super_ssn"]
-        DL_id = info["DL_id"]
-        cur.execute("""INSERT INTO employee (Fname, Minit, Lname, Bdate, Address, Sex, Salary, Super_ssn, DL_id) 
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-                    (fname, minit, lname, bdate, address, sex, salary, super_ssn, DL_id))
-        mysql.connection.commit()
+        required_fields = ["Fname", "Minit", "Lname", "Bdate", "Address", "Sex", "Salary", "Super_ssn", "DL_id"]
+        if not all(field in info for field in required_fields):
+            return make_response(jsonify({"Error": "Missing required fields"}), 400)
 
-        affected_rows = cur.rowcount
-        cur.close()
+        query = """INSERT INTO employee (Fname, Minit, Lname, Bdate, Address, Sex, Salary, Super_ssn, DL_id) 
+                   VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        args = (info["Fname"], info["Minit"], info["Lname"], info["Bdate"], info["Address"], info["Sex"], info["Salary"], info["Super_ssn"], info["DL_id"])
+        affected_rows = execute_query(query, args)
 
         return make_response(jsonify({"Message": "New Employee Added!", "Affected Rows": affected_rows}), 201)
     except Exception as e:
@@ -71,26 +71,19 @@ def add_employee():
 @app.route("/employees/<int:ssn>", methods=["PUT"])
 def update_employee(ssn):
     try:
-        cur = mysql.connection.cursor()
         info = request.get_json()
-        fname = info["Fname"]
-        minit = info["Minit"]
-        lname = info["Lname"]
-        bdate = info["Bdate"]
-        address = info["Address"]
-        sex = info["Sex"]
-        salary = info["Salary"]
-        super_ssn = info["Super_ssn"]
-        DL_id = info["DL_id"]
-        cur.execute("""UPDATE employee
-                    SET Fname = %s, Minit = %s, Lname = %s, Bdate = %s, Address = %s, Sex = %s, Salary = %s, Super_ssn = %s, DL_id = %s 
-                    WHERE ssn = %s""",
-                    (fname, minit, lname, bdate, address, sex, salary, super_ssn, DL_id, ssn))
-        mysql.connection.commit()
+        required_fields = ["Fname", "Minit", "Lname", "Bdate", "Address", "Sex", "Salary", "Super_ssn", "DL_id"]
+        if not all(field in info for field in required_fields):
+            return make_response(jsonify({"Error": "Missing required fields"}), 400)
 
-        affected_rows = cur.rowcount
-        cur.close()
+        query = """UPDATE employee
+                   SET Fname = %s, Minit = %s, Lname = %s, Bdate = %s, Address = %s, Sex = %s, Salary = %s, Super_ssn = %s, DL_id = %s 
+                   WHERE ssn = %s"""
+        args = (info["Fname"], info["Minit"], info["Lname"], info["Bdate"], info["Address"], info["Sex"], info["Salary"], info["Super_ssn"], info["DL_id"], ssn)
+        affected_rows = execute_query(query, args)
 
+        if affected_rows == 0:
+            return make_response(jsonify({"Error": "Employee not found"}), 404)
         return make_response(jsonify({"Message": "Employee Updated!", "Affected Rows": affected_rows}), 200)
     except Exception as e:
         return make_response(jsonify({"Error": str(e)}), 500)
@@ -98,13 +91,9 @@ def update_employee(ssn):
 @app.route("/employees/<int:ssn>", methods=["DELETE"])
 def delete_employee(ssn):
     try:
-        cur = mysql.connection.cursor()
-        cur.execute("DELETE FROM employee WHERE ssn = %s", (ssn,))
-        mysql.connection.commit()
-
-        affected_rows = cur.rowcount
-        cur.close()
-
+        affected_rows = execute_query("DELETE FROM employee WHERE ssn = %s", (ssn,))
+        if affected_rows == 0:
+            return make_response(jsonify({"Error": "Employee not found"}), 404)
         return make_response(jsonify({"Message": "Employee Deleted!", "Affected Rows": affected_rows}), 200)
     except Exception as e:
         return make_response(jsonify({"Error": str(e)}), 500)
